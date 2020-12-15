cb71df12e2a4c261f96428dd56d4f99a785b73f3
Bassam-home <bassammsmar@gmail.com>
2020-12-15 21:37:20 +0300

    STATIC_ROOT v2

--- store/settings.py
+++ store/settings.py
@@ -133,7 +133,7 @@ STATICFILES_DIRS = [
 ]
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'media/')
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
