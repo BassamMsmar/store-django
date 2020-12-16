063220842a306c10bbe22e610863f1ea122b38ac
Bassam-home <bassammsmar@gmail.com>
2020-12-16 21:51:00 +0300

    fix debog

--- store/prod_settings.py
+++ store/prod_settings.py
@@ -4,11 +4,9 @@ import dj_database_url
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
-debug_option = os.environ.get('DEBUG').lower()
-if debug_option == 'true':
-    DEBUG = True
-else:
-    DEBUG = False
+
+DEBUG = True
+
 
 ALLOWED_HOSTS = [
     '.herokuapp.com',
