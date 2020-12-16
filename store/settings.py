b3e36bf59ca3590d72753dad185e1fc260f8adff
Bassam-home <bassammsmar@gmail.com>
2020-12-16 21:33:21 +0300

    add store.prod_settings

--- store/settings.py
+++ store/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'ccfy(y%*r1e1n04iza1+__&$h9frm*djhpb3ae#utvpcs*tei4'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['store-django.herokuapp.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
